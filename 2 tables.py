094dc3dca366c7199243243fa23563ecc9f6a39f
Winchesters67 <110330276+Winchesters67@users.noreply.github.com>
2022-07-31 15:23:13 +0530

    Add files via upload

---
+++ 2 tables.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+data=pd.read_excel(r"C:\Users\HP\Documents\Book1.xlsx")
+
+
+# In[12]:
+
+
+data
+
+
+# In[22]:
+
+
+dat=pd.read_csv(r'C:\Users\HP\Documents\book.txt')
+
+
+# In[23]:
+
+
+dat
+
+
+# In[ ]:
+
+
+
+
