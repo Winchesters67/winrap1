7337941c8e2eac8e80c18407a577fe04b35c22e0
Winchesters67 <110330276+Winchesters67@users.noreply.github.com>
2022-07-31 15:53:03 +0530

    Add files via upload

---
+++ tab1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+pd.read_csv(r'C:\Users\HP\Documents\book.txt')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
